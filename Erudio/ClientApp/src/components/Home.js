878ef614326f4aa40f501b231de483f8cce5b74f
Sebastian Siwik <sebastian.siwik11@gmail.com>
2020-12-22 23:44:20 +0100

    Create homepage

--- Erudio/ClientApp/src/components/Home.js
+++ Erudio/ClientApp/src/components/Home.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
+import { Formik, Field, Form } from 'formik';
+import bubbles from '../images/bubbles.png';
+import swap from '../images/swap.svg';
+import image from '../images/image.svg';
+import '../App.css';
+import './Home.css';
+
+function BackgroundImage(props) {
+    return <img className='background right-side' src={bubbles} />;
+}
+
+function HeaderText(props) {
+    return (
+        <div>
+            <h1>Have words translated <strong>right</strong></h1>
+            <p className='text-right'>Benefit and contribute</p>
+            <p>Obtain highly accurate translations and contribute to help others!</p>
+        </div>
+    );
+}
+ 
+function TranslationRequestForm(props) {
+    return (
+        <Formik
+            initialValues={{ from: 'English', to: 'Polish', textToTranslate: '', context: '' }}
+            onSubmit={async values => {
+            await new Promise(resolve => setTimeout(resolve, 500));
+            alert(JSON.stringify(values, null, 2));
+            }}
+        >
+            <Form>
+                <div className='row-container'>
+                    <Field className='text-box small-box' name='from' placeholder='From' />
+                    <img src={swap} />
+                    <Field className='text-box small-box' name='to' placeholder='To' />
+                </div>
+                <Field className='text-box big-box' as='textarea' name='textToTranslate' placeholder='Get started!' />
+                <div className='row-container'>
+                    <Field className='text-box small-box' name='context' placeholder='Context' />
+                    <img className='image' src={image} />
+                </div>
+                <button type='submit'>Translate</button>
+            </Form>
+        </Formik>
+    );
+}
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+      <div className='row-container'>
+        <div className='left-side'>
+            <HeaderText />
+            <TranslationRequestForm />
+        </div>
+        <BackgroundImage />
       </div>
     );
   }
