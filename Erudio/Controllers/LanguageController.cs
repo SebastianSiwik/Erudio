a172244d8a5f3b2d1519ddc88ca1b7d84a59a83e
Sebastian Siwik <sebastian.siwik11@gmail.com>
2021-01-02 20:33:22 +0100

    Add language controller.

---
+++ Erudio/Controllers/LanguageController.cs
@@ -0,0 +1,55 @@
+﻿using Erudio.Data;
+using Erudio.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Erudio.Controllers
+{
+    [Route("api/languages")]
+    public class LanguageController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public LanguageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLanguages()
+        {
+            var languages = new List<LanguageViewModel>();
+            await  _context.Languages.ForEachAsync(l => languages.Add(new LanguageViewModel
+            {
+                LanguageId = l.LanguageId,
+                LanguageCode = l.LanguageCode,
+                LanguageName = l.LanguageName
+            }));
+
+            if (languages != null)
+            {
+                return Ok(languages);
+            }
+            return NotFound();
+        }
+
+        [Route("{languageCode}", Name = "GetLanguageByCode")]
+        [HttpGet]
+        public async Task<IActionResult> GetLanguageByCode(string languageCode)
+        {
+            var language = await _context.Languages.FirstOrDefaultAsync(x => x.LanguageCode == languageCode);
+
+            if (language != null)
+            {
+                return Ok(new LanguageViewModel
+                {
+                    LanguageId = language.LanguageId,
+                    LanguageCode = language.LanguageCode,
+                    LanguageName = language.LanguageName
+                });
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Erudio/ViewModels/LanguageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Erudio.ViewModels
+{
+    public class LanguageViewModel
+    {
+        public int LanguageId { get; set; }
+        public string LanguageCode { get; set; }
+        public string LanguageName { get; set; }
+    }
+}
